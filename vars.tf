4de205db5b4a4f35ccf65f1a23e00b2d68896b93
lmbalcao <lmbalcao@gmail.com>
2026-01-26 14:49:45 +0000

    +upd

---
+++ vars.tf
@@ -0,0 +1,26 @@
+variable "ssh_key" {
+    default = "ssh-rsa ....."
+}
+variable "api_url" {
+    # The Proxmox Web UI address, with /api2/json added to it.
+    default = "https://192.168.1.15:8006/api2/json" 
+}
+variable "proxmox_host" {
+    # The name of the Proxmox server listed under Datacenter
+    default = "..." 
+}
+variable "template_name" {
+  default = "debian-11-cloudinit-template"
+}
+variable "token_id" {
+  default = "terraform@pam!terraform_token_id"
+}
+variable "token_secret" {
+  default = "..." # Enter your API Secret here
+}
+variable "ipconfig_pihole" {
+  default = "ip=192.168.1.16/24,gw=192.168.1.1"
+}
+variable "ipconfig_homebridge" {
+  default = "ip=192.168.1.17/24,gw=192.168.1.1"
+}
