01132447faa9d9441a2cc41083b10269a1c915f1
lmbalcao <lmbalcao@gmail.com>
2026-01-26 15:26:41 +0000

    upd

--- main.tf
+++ main.tf
@@ -12,5 +12,5 @@ provider "proxmox" {
   pm_api_url = vars.api_url
   pm_api_token_id = var.token_id
   pm_api_token_secret = var.token_secret
-  pm_tls_insecure = true # Change to false if you have your 
+  pm_tls_insecure = true 
 }
